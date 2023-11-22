da2646acfb43f1d90a5628a6fab2fb3014f76cae
b.amusov <b.amusov@neurone.ru>
2023-11-22 16:07:07 +0300

    add exception_catcher

--- main.py
+++ main.py
@@ -17,19 +17,23 @@ async def on_startup(_):
 
 
 if __name__ == '__main__':
-    LOG_FILE_NAME = os.path.dirname(os.path.realpath(__file__)) + '/log/health_ai.log'
-    logger.add(
-        LOG_FILE_NAME,
-        level='DEBUG',
-        rotation='3 MB',
+    try:
+        LOG_FILE_NAME = os.path.dirname(os.path.realpath(__file__)) + '/log/health_ai.log'
+        logger.add(
+            LOG_FILE_NAME,
+            level='DEBUG',
+            rotation='3 MB',
 
-        backtrace=False,
-        catch=False,
-        diagnose=False,
-        compression='zip'
-    )
+            backtrace=False,
+            catch=False,
+            diagnose=False,
+            compression='zip'
+        )
 
-    logger.opt(exception=False)
+        logger.opt(exception=False)
 
-    with logger.contextualize(ip=None, path=None, method=None):
-        executor.start_polling(dp, on_startup=on_startup, skip_updates=True, timeout=None)
+        with logger.contextualize(ip=None, path=None, method=None):
+            executor.start_polling(dp, on_startup=on_startup, skip_updates=True, timeout=None)
+    except Exception as exc:
+        logger.error(exc)
+        raise Exception
